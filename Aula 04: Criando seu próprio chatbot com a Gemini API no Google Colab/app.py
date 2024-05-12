9610d4b83d24fe99b1331bfd7ee167a32c282e74
rafaelatn <119625304+rafaelatn@users.noreply.github.com>
2024-05-11 21:26:39 -0300

    Create app.py

---
+++ Aula 04: Criando seu próprio chatbot com a Gemini API no Google Colab/app.py
@@ -0,0 +1,40 @@
+# instalando o SDK do Google
+!pip install -q -U google-generativeai
+# importando o Python SDK
+import google.generativeai as genai
+GOOGLE_API_KEY= ('YOUR_GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
+# listando os modelos disponíveis
+for m in genai.list_models():
+  if 'generateContent' in m.supported_generation_methods:
+    print(m.name)
+# configurando o número de respostas e temperatura
+generation_config = {
+    'candidate_count': 1,
+    'temperature': 0.5,
+}
+# configurando diretrizes de segurança
+safety_settings = {
+    'HARASSMENT':'BLOCK_NONE',
+    'HATE': 'BLOCK_NONE',
+    'SEXUAL': 'BLOCK_NONE',
+    'DANGEROUS': 'BLOCK_NONE'
+}
+# inicializando o modelo
+model = genai.GenerativeModel(model_name='gemini-1.0-pro',
+                             generation_config=generation_config,
+                             safety_settings=safety_settings)
+# gerando resposta
+response = model.generate_content('INSERT_YOUR_PROMPT')
+print(response.text)
+# construindo chat
+chat = model.start_chat(history=[])
+
+prompt = input('Esperando prompt: ')
+
+while prompt != 'fim':
+  response = chat.send_message(prompt)
+  print('Resposta: ', response.text, '\n')
+  prompt = input('Esperando prompt: ')
+# acessar histórico do chat
+chat.history
